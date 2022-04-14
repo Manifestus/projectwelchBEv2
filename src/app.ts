2fbe7f064b167b29e9d93bd4ca770b30a9e5b8b4
Manifestus <link_figueroa20@hotmail.com>
2022-04-14 08:39:27 -0600

    change sync to auth

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ const app = createExpressServer({
 });
 //Sequelize Connector
 try {
-    sequelize.sync().then(() => {
+    sequelize.authenticate().then(() => {
     app.listen(process.env.PORT || 5000);
     app.get("/", (req: any, res: any) => {
       console.log(req)
