a6564099ca3c28abb6cc60772dd504bf471f604d
Manifestus <link_figueroa20@hotmail.com>
2022-04-14 06:46:56 -0600

    changed to correct nameing of controllers

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 import { sequelize } from "./models/index";
 import { createExpressServer } from "routing-controllers";
-import { ClientController } from "./controllers/client.controller";
-import { OrderController } from "./controllers/order.controller";
+import { ClientController } from "./controllers/client.Controller";
+import { OrderController } from "./controllers/order.Controller";
 import { OrderItemController } from "./controllers/orderitem.Controller";
 
 const port = 5000;
