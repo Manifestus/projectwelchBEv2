1fc47a73d2047641c3275e3a90da23c43a318582
Manifestus <link_figueroa20@hotmail.com>
2022-04-14 08:54:10 -0600

    test forced post

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,9 @@ require("dotenv").config();
 
 import { sequelize } from "./models/index";
 import { createExpressServer } from "routing-controllers";
-import { ClientController } from "./controllers/clientController"
+import { ClientController } from "./controllers/clientController";
 import { OrderController } from "./controllers/orderController";
-import { OrderItemController } from "./controllers/orderitemController"
+import { OrderItemController } from "./controllers/orderitemController";
 
 const port = 5000;
 
@@ -18,13 +18,16 @@ const app = createExpressServer({
 });
 //Sequelize Connector
 try {
-    sequelize.authenticate().then(() => {
+  sequelize.authenticate().then(() => {
     app.listen(process.env.PORT || 5000);
     app.get("/", (req: any, res: any) => {
-      console.log(req)
+      console.log(req);
       res.send("Hello World!");
     });
   });
-  } catch (error) {
-  
-}
+
+  app.post("/", (req: any, res: any) => {
+    console.log(req);
+    res.send("Hello World!");
+  });
+} catch (error) {}
