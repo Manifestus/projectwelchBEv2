04e9688aa953255d86a42c9dd266b0f89da69ee0
Manifestus <link_figueroa20@hotmail.com>
2022-04-14 07:00:36 -0600

    test

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,16 @@ require("dotenv").config();
 
 import { sequelize } from "./models/index";
 import { createExpressServer } from "routing-controllers";
-import { ClientController } from "./controllers/client.Controller"
-import { OrderController } from "./controllers/order.Controller";
-import { OrderItemController } from "./controllers/orderitem.Controller";
 
 const port = 5000;
 
 //Routing init
 console.info(`Starting server on http://localhost:${port}`);
 
-const routes = [ClientController, OrderController, OrderItemController];
+
 
 const app = createExpressServer({
-  controllers: routes,
+  controllers: [],
 });
 //Sequelize Connector
 try {
